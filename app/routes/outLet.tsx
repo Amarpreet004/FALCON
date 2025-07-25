803c4823854e18233d55bf8d6e4c8d6bb065d2c6
preet5216 <singhamarpreet455@gmail.com>
2025-07-25 16:30:40 +0530

    self

--- app/routes/outLet.tsx
+++ app/routes/outLet.tsx
@@ -2,7 +2,7 @@
 
 import React from 'react'
 import { Outlet } from "react-router-dom";
-import { NavbarDemo } from '../_components/navBar';
+import { NavbarDemo } from '../_components/newNav/Navnew';
 import Footer from '../_components/footer';
 
 const AppRoutes = () => {
